a92e8969ee720fd314695af4f48af0567d8757c4
NigelAntonPieter <na.pieter@hotmail.nl>
2022-09-22 15:50:06 +0200

    slideshow
    bezig met een tweede slideshow te maken met foto's van de school

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,7 @@
 <footer>
     <div class="wrapper">
         <h2>CONTACT</h2>
+        <p>Infopunt@curio.nl</p>
         
     </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,47 +11,69 @@
     <body>
         @include('header')
         <main>
+          <h2>Alles wat je moet weten </h2>
             <div class="grid-container">
                 <div class="info">
                     <div class="box1">
-                        <h3>Rooster</h3>
+                        
                     
 
                     </div>
                     <div class="box2">
-                        <h3>indeling van de blokken</h3>
+                        
                        
                     </div>
-                    <div class="box3">
-                        <h3>Versnellen</h3>
-                    
-
-                    </div>
+                   
                 </div>
+                
+               
+                
                 <div class="vakken">
-                    <div class="boxWIN large">
+                    <div class="container">
                         <h3>NATIVE</h3>
-                    
+                        <p><p>Je leert applicaties te bouwen die draaien op een Windows-computer. In dit blok werken we met de taal ‘Python’. Om die taal te leren, beginnen we met programmeren op een kleiner apparaat: de micro:bit. Voor meer informatie kijk je in de moduleboekjes.</p><p>
+                        <img src="img/phthon.jfif" alt="python" style="width:90px">
+                        <img src="img/Csharp.png" alt="C#" style="width:90px" >
 
                     </div>
-                    <div class="boxWEB large ">
+                    <div class="container">
                         <h3>WEB Development</h3>
-                    
+                        <p>Dit vak gaatover het maken van websites en webapplicaties. We beginnen simpel met de opmaaktalen HTML en CSS, dat zijn de bouwstenen van een website. Daarna leer je een interactieve website te bouwen.Voor meer informatie kijk je in de moduleboekjes</p>
+                        <img src="img/PHP.jfif" alt="PHP" style="width:90px">
+                        <img src="img/CSS,HTML.png" alt="HTML" style="width:90px">
+                        <img src="img/SQL.png" alt="SQL" style="width:90px">
 
                     </div>
-                    <div class="boxPRA large">
+                    <div class="container">
                         <h3>PRAKTIJK WERK</h3>
+                        <p>Je werkt zelfstandig aan de praktijkopdrachten in ons praktijklokaal. Omdat je zelfstandig moet werken, kun je niet altijd meteen je vragen stellen. Deze schrijf je dan op, om er bij de lessen PRO op terug te komen. Voor meer informatie kijk je in de boekjes van de praktijkopdrachten.</p>
+                        <img src="img/phthon.jfif" alt="python" style="width:90px">
+                        <img src="img/Csharp.png" alt="C#" style="width:90px" >
+                        <img src="img/PHP.jfif" alt="PHP" style="width:90px">
+                        <img src="img/CSS,HTML.png" alt="HTML" style="width:90px">
+                        <img src="img/SQL.png" alt="SQL" style="width:90px">
                     
 
                     </div>
-                    <div class="boxPRO large ">
+                    <div class="container">
                         <h3>PRAKTIJKONDERSTEUNING</h3>
-                    
+                        <p>Je krijgt coaching en ondersteuning bij de praktijkopdrachten. Ook is er les in modules die met het V-model te maken hebben. Meestal gaat dat niet over programmeren, maar juist over de andere kanten van ons vak.</p>
+                        
 
                     </div>
-                    <div class="boxGENE large">
+                    <div class="container">
                         <h3>NEDERLANDS/ENGELS/REKENEN</h3>
-                    
+                        <p>generieke vakken.</p>
+                        <img src="img/nederlands.jpg" alt="PHP" style="width:90px">
+                        <img src="img/optellen.png" alt="HTML" style="width:90px">
+                        <img src="img/engels.jpg" alt="SQL" style="width:90px">
+                    </div>
+
+                    <div class="container">
+                        <h3>ALGO</h3>
+                        <p>In deze les leer je gedachten gang van het coderen en daarbij gebruiken we vooral PHP</p>
+                        <img src="img/PHP.jfif" alt="PHP" style="width:90px">
+      
 
                     </div>
                 
@@ -62,195 +84,229 @@
                 
             </div>
 
-            <!-- <div class="teachers">
-                <div class="teacher">
-                    <img src="img/br10.jfif" alt="">
+           
+
+            
+            
+              <h2>Het docenten team en de:</h2>
+              <div class="slideshow-container">
+
+                <div class="mySlides fade">
+                  <div class="numbertext">1 / 8</div>
+                  <img src="img/docent1.jpg" style="width:100%">
+                  <div class="text">
                     <h4>Bart Roos </h4>
-	                <p>docent WIN/WEB/SLB/CGO</p>
-	                <p><a href="mailto:b.roos@curio.nl">b.roos@curio.nl</a></p> 
+                  <p>docent WIN/WEB/SLB/CGO</p>
+                  <p><a href="mailto:b.roos@curio.nl">b.roos@curio.nl</a></p> 
+                    </div>
                 </div>
-                <div class="teacher">
-                    <img src="img/eb89.jpg" alt="">
+
+                <div class="mySlides fade">
+                  <div class="numbertext">2 / 8</div>
+                  <img src="img/eb89.jpg" style="width:100%">
+                  <div class="text">
                     <h4>Elton Boekhoudt</h4>
-	                <p>docent WEB/TBZ</p>
-	                <p><a href="mailto:e.boekhoudt@curio.nl">e.boekhoudt@curio.nl</a></p>
+                  <p>docent WEB/TBZ</p>
+                  <p><a href="mailto:e.boekhoudt@curio.nl">e.boekhoudt@curio.nl</a></p>
+                  </div>
                 </div>
-                <div class="teacher">
-                    <img src="img/steven.png" alt="">
+
+                <div class="mySlides fade">
+                  <div class="numbertext">3 / 8</div>
+                  <img src="img/steven.png" style="width:100%">
+                  <div class="text">
                     <h4>Steven van Rosendaal</h4>
-	                <p>docent PRO/SLB/</p>
-	                <p><a href="mailto:s.vanrosendaal@curio.nl">s.vanrosendaal@curio.nl</a></p>
+                    <p>docent PRO/SLB/</p>
+                    <p><a href="mailto:s.vanrosendaal@curio.nl">s.vanrosendaal@curio.nl</a></p>
+                  </div>
                 </div>
-                <div class="teacher">
-                    <img src="img/ib81.jfif" alt="">
+
+                <div class="mySlides fade">
+                  <div class="numbertext">4 / 8</div>
+                  <img src="img/docent4.jpg"  style="width:100%">
+                  <div class="text">
                     <h4>Ine Vermeer </h4>
-	                <p>docent WEB/SLB</p>
-	                <p><a href="mailto:e.vermeer@curio.nl">e.vermeer@curio.nl</a></p>
+                    <p>docent WEB/SLB</p>
+                    <p><a href="mailto:e.vermeer@curio.nl">e.vermeer@curio.nl</a></p>
+                  </div>
                 </div>
-                <div class="teacher">
-                    <img src="img/wicher.png" alt="">
+
+                <div class="mySlides fade">
+                  <div class="numbertext">5 / 8</div>
+                  <img src="img/wicher.png" style="width:100%">
+                  <div class="text">
                     <h4>Wicher Hulzenbosch </h4>
-	                <p>docent WIN/PRA</p>
-	                <p><a href="mailto:w.hulzenbosch@curio.nl">w.hulzenbosch@curio.nl</a></p>
+                    <p>docent WIN/PRA</p>
+                    <p><a href="mailto:w.hulzenbosch@curio.nl">w.hulzenbosch@curio.nl</a></p>
+                  </div>
                 </div>
-                <div class="teacher">
-                    <img src="img/michel.png" alt="">
+
+                <div class="mySlides fade">
+                  <div class="numbertext">6 / 8</div>
+                  <img src="img/michel.png" style="width:100%">
+                  <div class="text">
                     <h4>Michel Meeuwesen</h4>
-	                <p>docent PRA</p>
-	                <p><a href="mailto:m.meeuwesen@curio.nl">m.meeuwesen@curio.nl</a></p>
+                    <p>docent PRA</p>
+                    <p><a href="mailto:m.meeuwesen@curio.nl">m.meeuwesen@curio.nl</a></p>
+                  </div>
                 </div>
-                <div class="teacher">
-                    <img src="img/Wiro.jfif" alt="">
+
+                <div class="mySlides fade">
+                  <div class="numbertext">7 / 8</div>
+                  <img src="img/Wiro.jfif" style="width:100%">
+                  <div class="text">
                     <h4>Wiro Verdiesen</h4>
                     <p>Docent Rekenen</p>
                     <p><a href="mailto:w.verdiesen@curio.nl">w.verdiesen@curio.nl</a></p>
+                  </div>
                 </div>
-                <div class="teacher">
-                    <img src="img/docent10.jfif" alt="">
+
+                <div class="mySlides fade">
+                  <div class="numbertext">8 / 8</div>
+                  <img src="img/docent10.jfif" style="width:100%">
+                  <div class="text">
                     <h4>Vincent Hooft</h4>
                     <p>Docent ALGO</p>
                     <p><a href="mailto:v.hooft@curio.nl">v.hooft@curio.nl</a></p>
+                  </div>
                 </div>
-            </div> -->
-
-            
-
-
-<div class="slideshow-container">
-
-<div class="mySlides fade">
-  <div class="numbertext">1 / 8</div>
-  <img src="img/docent1.jpg" style="width:100%">
-  <div class="text">
-    <h4>Bart Roos </h4>
-	<p>docent WIN/WEB/SLB/CGO</p>
-	<p><a href="mailto:b.roos@curio.nl">b.roos@curio.nl</a></p> 
-    </div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">2 / 8</div>
-  <img src="img/eb89.jpg" style="width:100%">
-  <div class="text">
-    <h4>Elton Boekhoudt</h4>
-	<p>docent WEB/TBZ</p>
-	<p><a href="mailto:e.boekhoudt@curio.nl">e.boekhoudt@curio.nl</a></p>
-  </div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">3 / 8</div>
-  <img src="img/steven.png" style="width:100%">
-  <div class="text">
-    <h4>Steven van Rosendaal</h4>
-    <p>docent PRO/SLB/</p>
-    <p><a href="mailto:s.vanrosendaal@curio.nl">s.vanrosendaal@curio.nl</a></p>
-  </div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">4 / 8</div>
-  <img src="img/docent4.jpg"  style="width:100%">
-  <div class="text">
-    <h4>Ine Vermeer </h4>
-    <p>docent WEB/SLB</p>
-    <p><a href="mailto:e.vermeer@curio.nl">e.vermeer@curio.nl</a></p>
-  </div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">5 / 8</div>
-  <img src="img/wicher.png" style="width:100%">
-  <div class="text">
-    <h4>Wicher Hulzenbosch </h4>
-    <p>docent WIN/PRA</p>
-    <p><a href="mailto:w.hulzenbosch@curio.nl">w.hulzenbosch@curio.nl</a></p>
-  </div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">6 / 8</div>
-  <img src="img/michel.png" style="width:100%">
-  <div class="text">
-    <h4>Michel Meeuwesen</h4>
-    <p>docent PRA</p>
-    <p><a href="mailto:m.meeuwesen@curio.nl">m.meeuwesen@curio.nl</a></p>
-  </div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">7 / 8</div>
-  <img src="img/Wiro.jfif" style="width:100%">
-  <div class="text">
-    <h4>Wiro Verdiesen</h4>
-    <p>Docent Rekenen</p>
-    <p><a href="mailto:w.verdiesen@curio.nl">w.verdiesen@curio.nl</a></p>
-  </div>
-</div>
-
-<div class="mySlides fade">
-  <div class="numbertext">8 / 8</div>
-  <img src="img/docent10.jfif" style="width:100%">
-  <div class="text">
-    <h4>Vincent Hooft</h4>
-    <p>Docent ALGO</p>
-    <p><a href="mailto:v.hooft@curio.nl">v.hooft@curio.nl</a></p>
-  </div>
-</div>
 
 
 
 
-<a class="prev" onclick="plusSlides(-1)">❮</a>
-<a class="next" onclick="plusSlides(1)">❯</a>
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" onclick="plusSlides(1)">❯</a>
 
-</div>
-<br>
+                </div>
+                <br>
 
-<div style="text-align:center">
-  <span class="dot" onclick="currentSlide(1)"></span> 
-  <span class="dot" onclick="currentSlide(2)"></span> 
-  <span class="dot" onclick="currentSlide(3)"></span> 
-  <span class="dot" onclick="currentSlide(4)"></span> 
-  <span class="dot" onclick="currentSlide(5)"></span> 
-  <span class="dot" onclick="currentSlide(6)"></span>
-  <span class="dot" onclick="currentSlide(7)"></span> 
-  <span class="dot" onclick="currentSlide(8)"></span> 
-  
-</div>
+                <div style="text-align:center">
+                  <span class="dot" onclick="currentSlide(1)"></span> 
+                  <span class="dot" onclick="currentSlide(2)"></span> 
+                  <span class="dot" onclick="currentSlide(3)"></span> 
+                  <span class="dot" onclick="currentSlide(4)"></span> 
+                  <span class="dot" onclick="currentSlide(5)"></span> 
+                  <span class="dot" onclick="currentSlide(6)"></span>
+                  <span class="dot" onclick="currentSlide(7)"></span> 
+                  <span class="dot" onclick="currentSlide(8)"></span> 
+                  
+                </div>
 
-<script>
-let slideIndex = 1;
-showSlides(slideIndex);
+                <script>
+                let slideIndex = 1;
+                showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+                function plusSlides(n) {
+                  showSlides(slideIndex += n);
+                }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+                function currentSlide(n) {
+                  showSlides(slideIndex = n);
+                }
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-</script>
+                function showSlides(n) {
+                  let i;
+                  let slides = document.getElementsByClassName("mySlides");
+                  let dots = document.getElementsByClassName("dot");
+                  if (n > slides.length) {slideIndex = 1}    
+                  if (n < 1) {slideIndex = slides.length}
+                  for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";  
+                  }
+                  for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                  }
+                  slides[slideIndex-1].style.display = "block";  
+                  dots[slideIndex-1].className += " active";
+                }
+                </script>
+              </div>
+              <!-- <div class="slideshow-container">
+
+                <div class="mySlide1 fade">
+                  <div class="numbertext">1 / 4</div>
+                    <img src="img/locatie1.jpeg">
+                  
+                  
+                </div>
+
+                <div class="mySlide1 fade">
+                  <div class="numbertekst">2 / 4</div>
+                    <img src="img/locatie2.jpeg">
+                  
+                  
+                </div>
+
+                <div class="mySlide1 fade">
+                  <div class="numbertekst">3 / 4</div>
+                    <img src="img/locatie3.jpeg">
+                  
+                  
+                </div>
+
+                <div class="mySlide1 fade">
+                  <div class="numbertekst">4 / 4</div>
+                    <img src="img/locatie4.jpeg">
+                  
+                  
+                </div>
+
+                
+                
                 
 
-        </main>
-        @include('footer')    
+
+                <a class="prev" onclick="plusSlides(-1)">❮</a>
+                <a class="next" onclick="plusSlides(1)">❯</a>
+
+                </div>
+                <br>
+
+                <div style="text-align:center">
+                  <span class="dote" onclick="currentSlide(1)"></span> 
+                  <span class="dote" onclick="currentSlide(2)"></span> 
+                  <span class="dote" onclick="currentSlide(3)"></span> 
+                  <span class="dote" onclick="currentSlide(4)"></span> 
+                  
+                  
+                </div>
+
+                <script>
+                let slideIndex = 1;
+                showSlides(slideIndex);
+
+                function plusSlides(n) {
+                  showSlides(slideIndex += n);
+                }
+
+                function currentSlide(n) {
+                  showSlides(slideIndex = n);
+                }
+
+                function showSlides(n) {
+                  let i;
+                  let slides = document.getElementsByClassName("mySlide1");
+                  let dots = document.getElementsByClassName("dote");
+                  if (n > slides.length) {slideIndex = 1}    
+                  if (n < 1) {slideIndex = slides.length}
+                  for (i = 0; i < slides.length; i++) {
+                    slides[i].style.display = "none";  
+                  }
+                  for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                  }
+                  slides[slideIndex-1].style.display = "block";  
+                  dots[slideIndex-1].className += " active";
+                }
+                </script> -->
+              
+            </div>
             
+
+                            
+
+          </main>
+          @include('footer')    
+                        
     </body>
 </html>
