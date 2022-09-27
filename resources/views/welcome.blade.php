56eaa609b20c613eae6ce167dbf2cef56091dac4
NigelAntonPieter <na.pieter@hotmail.nl>
2022-09-27 11:24:48 +0200

    Model images
    een model images toegevoegd zodat als je op het rooster img klikt dat hij wordt uitvergroot

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,7 +17,43 @@
                     <div class="box1">
                         <h2>Rooster</h2>
                         <P>Dit het rooster want je kan verwachten in het 1e jaar</P>
-                        <img src="img/rooster.png" alt="rooster" style = "width:730px">
+                        <img id="myImg" src="img/Rooster.png" alt="Rooster" style="width:100%;max-width:300px">
+
+                        <!-- The Modal -->
+                        <div id="myModal" class="modal">
+
+                          <!-- The Close Button -->
+                          <span class="close">&times;</span>
+
+                          <!-- Modal Content (The Image) -->
+                          <img class="modal-content" id="img01">
+
+                          <!-- Modal Caption (Image Text) -->
+                          <div id="caption"></div>
+                        </div>
+                        <script>
+                        // Get the modal
+                        var modal = document.getElementById("myModal");
+
+                        // Get the image and insert it inside the modal - use its "alt" text as a caption
+                        var img = document.getElementById("myImg");
+                        var modalImg = document.getElementById("img01");
+                        var captionText = document.getElementById("caption");
+                        img.onclick = function(){
+                          modal.style.display = "block";
+                          modalImg.src = this.src;
+                          captionText.innerHTML = this.alt;
+                        }
+
+                        // Get the <span> element that closes the modal
+                        var span = document.getElementsByClassName("close")[0];
+
+                        // When the user clicks on <span> (x), close the modal
+                        span.onclick = function() { 
+                          modal.style.display = "none";
+                        }
+                        </script>
+
                     
 
                     </div>
